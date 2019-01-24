31ff14f8f7944aa83bfe8a9582620a1466002409
cbardzinsky <cbardzinsky-ext@cci-paris-idf.fr>
2019-01-24 10:48:51 +0100

    Menu + carousel

--- app/initialize.js
+++ app/initialize.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   // do your setup here
   console.log('Initialized app');
 });
+
+$(document).ready(function(){
+	$("#close-button").click(function(){
+		$("#bloc-balades").hide();
+	});
+});
